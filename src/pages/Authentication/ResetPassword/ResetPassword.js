1cddc72870c6320ef3c47191e610cc3ee73a668a
trandung070899 <trandung070899@gmail.com>
2021-09-28 11:11:53 +0700

    update code

--- src/pages/Authentication/ResetPassword/ResetPassword.js
+++ src/pages/Authentication/ResetPassword/ResetPassword.js
@@ -5,36 +5,7 @@ import "./ResetPassword.scss";
 const ResetPassword = () => {
   return (
     <AuthLayout>
-      <div className="wrap-reset-pass">
-        <div className="col-md-3 col-sm-3 m-auto content-reset-pass">
-          <h2>Thay đổi mật khẩu</h2>
-          <form>
-            <div className="form-group">
-              <label>Mật khẩu mới</label>
-              <input
-                type="password"
-                name="newpassword"
-                className="form-control"
-                id="newpassword"
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label>Xác nhận mật khẩu</label>
-              <input
-                type="password"
-                name="confirmpassword"
-                className="form-control"
-                id="confirmpassword"
-                required
-              />
-            </div>
-            <button type="submit" className="btn btn-primary float-right">
-              Xác nhận
-            </button>
-          </form>
-        </div>
-      </div>
+      <div>Reset password</div>
     </AuthLayout>
   );
 };
