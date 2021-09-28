4ba946753ed6370aa02f13c6dc210f6243a00392
trandung070899 <trandung070899@gmail.com>
2021-09-28 10:57:44 +0700

    fix responsive

--- src/pages/Authentication/ResetPassword/ResetPassword.js
+++ src/pages/Authentication/ResetPassword/ResetPassword.js
@@ -1,10 +1,40 @@
 import AuthLayout from "layout/AuthLayout/AuthLayout";
 import React from "react";
+import "./ResetPassword.scss";
 
 const ResetPassword = () => {
   return (
     <AuthLayout>
-      <div>Reset password</div>
+      <div className="wrap-reset-pass">
+        <div className="col-md-3 col-sm-3 m-auto content-reset-pass">
+          <h2>Thay đổi mật khẩu</h2>
+          <form>
+            <div className="form-group">
+              <label>Mật khẩu mới</label>
+              <input
+                type="password"
+                name="newpassword"
+                className="form-control"
+                id="newpassword"
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label>Xác nhận mật khẩu</label>
+              <input
+                type="password"
+                name="confirmpassword"
+                className="form-control"
+                id="confirmpassword"
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-primary float-right">
+              Xác nhận
+            </button>
+          </form>
+        </div>
+      </div>
     </AuthLayout>
   );
 };
