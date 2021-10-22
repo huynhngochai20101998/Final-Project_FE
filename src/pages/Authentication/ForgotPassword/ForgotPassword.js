d811cc01f483338620ec0c20e6607d17142d6df0
trandung070899 <trandung070899@gmail.com>
2021-10-22 18:18:34 +0700

    update code

--- src/pages/Authentication/ForgotPassword/ForgotPassword.js
+++ src/pages/Authentication/ForgotPassword/ForgotPassword.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AuthLayout from "layout/AuthLayout/AuthLayout";
 import "./ForgotPassword.scss";
 import * as Yup from "yup";
 import TextField from "./TextField";
 import { Form, Formik } from "formik";
+import axios from "axios";
 
 const ForgotPassword = () => {
+  const [existUser, setExistUser] = useState([]); // lấy ra mảng user
+  const [logError, setLogError] = useState(""); //nếu tồn tại email thông báo email tồn tại, còn không trả về ""
+  const [checkClick, setCheckClick] = useState("");
   const validate = Yup.object({
     email: Yup.string().email("email không hợp lệ").required("Nhập vào email")
   });
+
+  function handleSubmit(values) {
+    console.log(values.email);
+    handleExistUser(values);
+    if (!handleExistUser(values)) {
+      console.log(values);
+    }
+  }
+
+  function handleExistUser(values) {
+    const checkUser = existUser.some((item) => {
+      return item.email === values.email;
+    });
+    if (checkUser) {
+      setLogError("Đã gửi email. Vui lòng kiểm tra hòm thư của bạn");
+      setCheckClick("");
+    } else {
+      setLogError("Email không tồn tại");
+    }
+    return checkUser;
+  }
+
+  function onclickinput() {
+    setCheckClick("true");
+    setLogError("");
+  }
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/posts")
+      .then((response) => {
+        const listUser = response.data;
+        setExistUser(listUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <AuthLayout>
@@ -17,9 +59,7 @@ const ForgotPassword = () => {
           email: ""
         }}
         validationSchema={validate}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
+        onSubmit={(values) => handleSubmit(values)}
       >
         {() => (
           <Form className="container forgot-form">
@@ -33,6 +73,9 @@ const ForgotPassword = () => {
                   name="email"
                   type="email"
                   className="form-group"
+                  errormess={logError}
+                  onClick={onclickinput}
+                  checkclick={checkClick}
                 />
                 <div className="col-md-12 text-center">
                   <button
--- src/pages/Authentication/ForgotPassword/TextField.js
+++ src/pages/Authentication/ForgotPassword/TextField.js
@@ -4,20 +4,33 @@ import { ErrorMessage, useField } from "formik";
 function TextField({ label, className, ...props }) {
   const [field, meta] = useField(props);
   return (
-    <div
-      className={`${className} ${meta.touched && meta.error && "is-invalid"}`}
-    >
-      <div className="form-check">
-        <input className="form-check-input" {...field} {...props} />
-        <label className="form-check-label" htmlFor={field.name}>
-          {label}
-        </label>
-      </div>
+    <div className={className}>
+      <label htmlFor={field.name}>{label}</label>
+      <input
+        className={`form-control ${meta.touched && meta.error && "is-invalid"}`}
+        {...field}
+        {...props}
+        autoComplete="off"
+      />
       <ErrorMessage
         component="div"
         name={field.name}
-        className="active_failure"
+        className="active-failure"
       ></ErrorMessage>
+      <span
+        htmlFor={field.name}
+        className={
+          props.errormess === "Email không tồn tại"
+            ? "active-failure"
+            : "active-success"
+        }
+        id="label-error"
+      >
+        {(props.errormess && props.checkClick) ||
+        (props.errormess && field.value !== "")
+          ? props.errormess
+          : ""}
+      </span>
     </div>
   );
 }
