5de6b44af0c3d1ec2779b2f8660f147b17598590
trandung070899 <trandung070899@gmail.com>
2021-10-26 09:19:11 +0700

    push code

--- src/pages/Authentication/ResetPassword/ResetPassword.js
+++ src/pages/Authentication/ResetPassword/ResetPassword.js
@@ -1,5 +1,5 @@
 import AuthLayout from "layout/AuthLayout/AuthLayout";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Formik, Form } from "formik";
 import "./ResetPassword.scss";
 import TextField from "./TextField";
@@ -9,81 +9,34 @@ import { useHistory } from "react-router-dom";
 
 const ResetPassword = () => {
   const history = useHistory();
-  const [existUser, setExistUser] = useState([]); // lấy ra mảng user
-  const [logError, setLogError] = useState(""); //nếu tồn tại email thông báo email tồn tại, còn không trả về ""
-  const [checkClick, setCheckClick] = useState("");
-  const errors = "Email đã tồn tại";
-  console.log(logError, checkClick);
   const validate = Yup.object({
-    firstName: Yup.string().required("Tên bạn là gì?"),
-    lastName: Yup.string().required("Tên bạn là gì?"),
-    email: Yup.string().email("email không hợp lệ").required("Nhập vào email"),
     password: Yup.string()
       .min(5, "Mật khẩu ít nhất 5 ký tự")
       .required("Nhập vào mật khẩu"),
     confirmPassword: Yup.string()
       .oneOf([Yup.ref("password"), null], "Mật khẩu không khớp")
-      .required("Xác nhận lại mật khẩu của bạn"),
-    termsAndConditions: Yup.bool().oneOf([true], "Yêu cầu chấp nhận điều khoản")
+      .required("Xác nhận lại mật khẩu của bạn")
   });
 
   function handleSubmit(values) {
-    handleExistUser(values);
-    if (!handleExistUser(values)) {
-      const { termsAndConditions, ...dataUser } = values;
-      console.log(termsAndConditions);
-      axios
-        .post("http://localhost:3000/user", dataUser)
-        .then((response) => {
-          console.log(response);
-          history.push("/signin");
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }
-  }
-
-  function handleExistUser(values) {
-    const checkUser = existUser.some((item) => {
-      return item.email === values.email;
-    });
-    if (checkUser) {
-      setLogError(errors);
-      setCheckClick("");
-    } else {
-      setLogError("");
-    }
-    return checkUser;
-  }
-
-  // function onclickinput() {
-  //   setCheckClick("true");
-  //   setLogError("");
-  // }
-
-  useEffect(() => {
+    console.log(values);
     axios
-      .get("http://localhost:3000/user")
+      .put("http://localhost:3000/user/1", values)
       .then((response) => {
-        const listUser = response.data;
-        setExistUser(listUser);
+        console.log(response);
+        history.push("/signin");
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }
 
   return (
     <AuthLayout>
       <Formik
         initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
           password: "",
-          confirmPassword: "",
-          termsAndConditions: false
+          confirmPassword: ""
         }}
         validationSchema={validate}
         onSubmit={(values) => handleSubmit(values)}
@@ -91,7 +44,7 @@ const ResetPassword = () => {
         {() => (
           <Form className="container reset-form">
             <div className="reset-header">
-              <h3>ĐĂNG KÝ</h3>
+              <h3>ĐỔI MẬT KHẨU</h3>
             </div>
             <div className="reset-content">
               <TextField
