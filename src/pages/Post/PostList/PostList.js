85b6bb1c0168751fa47a6eae9ac4e8cc35c61751
Duc Huy <nguyenduchuy548@gmail.com>
2021-10-27 23:29:08 +0700

    test api fake render post

--- src/pages/Post/PostList/PostList.js
+++ src/pages/Post/PostList/PostList.js
@@ -1,34 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "reactstrap";
-
+import Axios from "axios";
 import "./PostList.scss";
 export default function PostList() {
+  const [postList, setPostList] = useState([]);
+  useEffect(() => {
+    async function getDataList() {
+      try {
+        const api = Axios.create({
+          baseURL: "http://localhost:3000"
+        });
+        const response = await api.get(`/posts`);
+        setPostList(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getDataList();
+  }, []);
+
   return (
     <div className="PostList">
-      <div className="PostList__form">
-        <div className="PostList__form__info-user">
-          <div>
-            <img src="https://via.placeholder.com/256x186?fbclid=IwAR18p3QwgMQ0wYEmlIqxKZFbDBTFAhNZD8R4VyH6DxWdI6GULxDei-7L87M" />
-            <p>Nguyễn Dũng</p>
+      {postList.map((post) => {
+        return (
+          <div className="PostList__form" key={post.id}>
+            <div className="PostList__form__info-user">
+              <div>
+                <img src="https://via.placeholder.com/256x186?fbclid=IwAR18p3QwgMQ0wYEmlIqxKZFbDBTFAhNZD8R4VyH6DxWdI6GULxDei-7L87M" />
+                <p>Nguyễn Dũng</p>
+              </div>
+              <div>20/10/2021</div>
+            </div>
+            <div className="PostList__form__info-post">
+              <h5>{post.topic}</h5>
+              <p>Số lượng thành viên {post.numberMember} người</p>
+              <p>{post.content}</p>
+            </div>
+            <div className="PostList__form__see-more">
+              <Button>Xem thêm</Button>
+            </div>
           </div>
-          <div>20/10/2021</div>
-        </div>
-        <div className="PostList__form__info-post">
-          <h5>Tìm nhóm học chung JS</h5>
-          <p>Số lượng thành viên 5 người</p>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry Lorem Ipsum has been the industryss standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged.
-          </p>
-        </div>
-        <div className="PostList__form__see-more">
-          <Button>Xem thêm</Button>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 }
