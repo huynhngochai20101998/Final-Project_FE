956b3983de154ba2dc25e66478009489bd8f908c
trandung070899 <trandung070899@gmail.com>
2021-10-28 09:43:34 +0700

    fix code

--- src/pages/Authentication/SignUp/TextField.js
+++ src/pages/Authentication/SignUp/TextField.js
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { ErrorMessage, useField } from "formik";
 
-function TextField({ label, className, ...props }) {
+function TextField({ label, className, type, ...props }) {
   const [field, meta] = useField(props);
+  const [isShowPassword, setIsShowPassword] = useState("");
 
   return (
     <div className={className}>
       <label htmlFor={field.name}>{label}</label>
       <input
-        className={`form-control ${meta.touched && meta.error && "is-invalid"}`}
+        className={`form-control ${meta.touched && meta.error} ${
+          type === "password" ? "password" : ""
+        }`}
+        type={isShowPassword ? "text" : type}
         {...field}
         {...props}
         autoComplete="off"
       />
-      <span htmlFor={field.name} className="active-failure" id="label-error">
-        {(props.errormess && props.checkClick) ||
-        (props.errormess && field.value !== "")
-          ? props.errormess
-          : ""}
-      </span>
+      {type === "password" ? (
+        <div
+          className="show-password"
+          onClick={() => setIsShowPassword(!isShowPassword)}
+        >
+          {isShowPassword ? (
+            <i className="far fa-eye" />
+          ) : (
+            <i className="far fa-eye-slash" />
+          )}
+        </div>
+      ) : (
+        ""
+      )}
       <ErrorMessage
         component="div"
         name={field.name}
