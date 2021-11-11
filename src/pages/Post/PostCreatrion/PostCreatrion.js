9ab8e0f5da3973421e0f562364cade6539a0db18
Duc Huy <nguyenduchuy548@gmail.com>
2021-11-11 22:26:07 +0700

    update

--- src/pages/Post/PostCreatrion/PostCreatrion.js
+++ src/pages/Post/PostCreatrion/PostCreatrion.js
@@ -32,175 +32,157 @@ const PostCreatrion = () => {
   return (
     <HomeLayout>
       <div className="PostCreate">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-2"></div>
-            <div className="col-md-8">
-              <div className="PostCreate__form">
-                <div className="PostCreate__form__title">
-                  <h3>Đăng bài viết</h3>
-                </div>
-                <div className="PostCreate__form__content">
-                  <Formik
-                    initialValues={{
+        <div className="PostCreate__form">
+          <div className="PostCreate__form__title">
+            <h3>Đăng bài viết</h3>
+          </div>
+          <div className="PostCreate__form__content">
+            <Formik
+              initialValues={{
+                title: "",
+                topic_id: "",
+                sessions: 1,
+                members: 3,
+                sumWeek: 1,
+                content: ""
+              }}
+              validationSchema={validationSchema}
+              onSubmit={(values, actions) => {
+                const formatValue = {
+                  ...values,
+                  topic_id: Number(values.topic_id)
+                };
+                http.post("/api/posts", formatValue).then(() => {
+                  alert("Bạn đã đăng thành công");
+                  actions.setSubmitting(false);
+                  actions.resetForm({
+                    values: {
                       title: "",
                       topic_id: "",
                       sessions: 1,
                       members: 3,
                       sumWeek: 1,
                       content: ""
-                    }}
-                    validationSchema={validationSchema}
-                    onSubmit={(values, actions) => {
-                      const formatValue = {
-                        ...values,
-                        topic_id: Number(values.topic_id)
-                      };
-                      http.post("/api/posts", formatValue).then(() => {
-                        alert("Bạn đã đăng thành công");
-                        actions.setSubmitting(false);
-                        actions.resetForm({
-                          values: {
-                            title: "",
-                            topic_id: "",
-                            sessions: 1,
-                            members: 3,
-                            sumWeek: 1,
-                            content: ""
-                          }
-                        });
-                        history.push("/");
-                      });
-                      console.log(formatValue);
-                    }}
-                  >
-                    {(formikProps) => {
-                      const {
-                        values,
-                        errors,
-                        touched,
-                        handleChange,
-                        handleBlur,
-                        isSubmitting
-                      } = formikProps;
-                      return (
-                        <Form>
-                          {isLoading ? (
-                            <>
-                              <FastField
-                                name="title"
-                                component={InputField}
-                                label="Tiêu đề"
-                                placeholder=""
+                    }
+                  });
+                  history.push("/");
+                });
+                console.log(formatValue);
+              }}
+            >
+              {(formikProps) => {
+                const {
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  isSubmitting
+                } = formikProps;
+                return (
+                  <Form>
+                    {isLoading ? (
+                      <>
+                        <FastField
+                          name="title"
+                          component={InputField}
+                          label="Tiêu đề"
+                          placeholder=""
+                        />
+                        <div className="PostCreate__form__content__select">
+                          <div className="PostCreate__form__content__select__topic">
+                            <FormGroup>
+                              <Label for="exampleSelect">Chủ đề</Label>
+                              <Input
+                                name="topic_id"
+                                type="select"
+                                value={values.topic_id}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                // defaultValue=""
+                                invalid={
+                                  errors["topic_id"] && touched["topic_id"]
+                                }
+                              >
+                                <option value="" disabled label="Chọn chủ đề" />
+                                <option value="1" label="java" />
+                                <option value="2" label="javascript" />
+                                <option value="3" label="Nestjs" />
+                              </Input>
+                              <ErrorMessage
+                                name={"topic_id"}
+                                component={FormFeedback}
                               />
-                              <div className="PostCreate__form__content__select">
-                                <div className="PostCreate__form__content__select__topic">
-                                  <FormGroup>
-                                    <Label for="exampleSelect">Chủ đề</Label>
-                                    <Input
-                                      name="topic_id"
-                                      type="select"
-                                      value={values.topic_id}
-                                      onChange={handleChange}
-                                      onBlur={handleBlur}
-                                      // defaultValue=""
-                                      invalid={
-                                        errors["topic_id"] &&
-                                        touched["topic_id"]
-                                      }
-                                    >
-                                      <option
-                                        value=""
-                                        disabled
-                                        label="Chọn chủ đề"
-                                      />
-                                      <option value="1" label="java" />
-                                      <option value="2" label="javascript" />
-                                      <option value="3" label="Nestjs" />
-                                    </Input>
-                                    <ErrorMessage
-                                      name={"topic_id"}
-                                      component={FormFeedback}
-                                    />
-                                  </FormGroup>
-                                </div>
-                                <div className="PostCreate__form__content__select__number">
-                                  <FastField
-                                    name="sessions"
-                                    component={InputField}
-                                    label="Số buổi/tuần"
-                                    placeholder=""
-                                    type="number"
-                                  />
-                                  <FastField
-                                    name="members"
-                                    component={InputField}
-                                    label="Số thành viên"
-                                    placeholder=""
-                                    type="number"
-                                  />
-                                  <FastField
-                                    name="sumWeek"
-                                    component={InputField}
-                                    label="Tổng số tuần học"
-                                    placeholder=""
-                                    type="number"
-                                  />
-                                </div>
-                              </div>
-                              <FastField
-                                name="content"
-                                component={InputField}
-                                label="Nội dung"
-                                placeholder=""
-                                type="textarea"
-                              />
-                              <div className="PostCreate__form__button">
-                                <Button
-                                  type="reset"
-                                  onClick={() => history.push("/")}
-                                >
-                                  Hủy
-                                </Button>
-                                <Button
-                                  onClick={() => setIsLoading(!isLoading)}
-                                >
-                                  Tiếp tục
-                                </Button>
-                              </div>
-                            </>
-                          ) : (
-                            <>
-                              <div className="PostCreate__form__content">
-                                <Label for="exampleSelect">
-                                  Thời gian rảnh
-                                </Label>
-
-                                <div className="PostCreate__form__button">
-                                  <Button
-                                    type="reset"
-                                    onClick={() => setIsLoading(!isLoading)}
-                                  >
-                                    Quay lại
-                                  </Button>
-                                  <Button type="submit">
-                                    {isSubmitting && (
-                                      <Spinner color="light" size="sm" />
-                                    )}
-                                    Hoàn tất
-                                  </Button>
-                                </div>
-                              </div>
-                            </>
-                          )}
-                        </Form>
-                      );
-                    }}
-                  </Formik>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-2"></div>
+                            </FormGroup>
+                          </div>
+                          <div className="PostCreate__form__content__select__number">
+                            <FastField
+                              name="sessions"
+                              component={InputField}
+                              label="Số buổi/tuần"
+                              placeholder=""
+                              type="number"
+                            />
+                            <FastField
+                              name="members"
+                              component={InputField}
+                              label="Số thành viên"
+                              placeholder=""
+                              type="number"
+                            />
+                            <FastField
+                              name="sumWeek"
+                              component={InputField}
+                              label="Tổng số tuần học"
+                              placeholder=""
+                              type="number"
+                            />
+                          </div>
+                        </div>
+                        <FastField
+                          name="content"
+                          component={InputField}
+                          label="Nội dung"
+                          placeholder=""
+                          type="textarea"
+                        />
+                        <div className="PostCreate__form__button">
+                          <Button
+                            type="reset"
+                            onClick={() => history.push("/")}
+                          >
+                            Hủy
+                          </Button>
+                          <Button onClick={() => setIsLoading(!isLoading)}>
+                            Tiếp tục
+                          </Button>
+                        </div>
+                      </>
+                    ) : (
+                      <>
+                        <div className="PostCreate__form__content">
+                          <Label for="exampleSelect">Thời gian rảnh</Label>
+                          <div className="PostCreate__form__button">
+                            <Button
+                              type="reset"
+                              onClick={() => setIsLoading(!isLoading)}
+                            >
+                              Quay lại
+                            </Button>
+                            <Button type="submit">
+                              {isSubmitting && (
+                                <Spinner color="light" size="sm" />
+                              )}
+                              Hoàn tất
+                            </Button>
+                          </div>
+                        </div>
+                      </>
+                    )}
+                  </Form>
+                );
+              }}
+            </Formik>
           </div>
         </div>
       </div>
