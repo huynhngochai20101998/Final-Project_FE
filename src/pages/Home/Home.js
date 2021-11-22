9a2e8900904d0d30c152b8d5d679c1821d254b1d
Duc Huy <nguyenduchuy548@gmail.com>
2021-11-22 22:18:37 +0700

    layout white-board , fix loading home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,17 +5,19 @@ import HomeLayout from "layout/HomeLayout/HomeLayout";
 import "./Home.scss";
 import { Link } from "react-router-dom";
 import Loading from "components/Loading/Loading";
+import InfinitScroll from "react-infinite-scroll-component";
 import moment from "moment";
 
 const Home = () => {
   const [postList, setPostList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [noMore, setNoMore] = useState(true);
+  const [page, setPage] = useState(2);
 
   useEffect(() => {
     async function getDataList() {
       try {
-        const response = await http.get("/api/posts");
-
+        const response = await http.get("/api/posts?page=1");
         setPostList(response.data.data);
         setIsLoading(false);
       } catch (err) {
@@ -25,6 +27,19 @@ const Home = () => {
     getDataList();
   }, []);
 
+  const fetchPosts = async () => {
+    const res = await http.get(`/api/posts?page=${page}`);
+    return res.data.data;
+  };
+
+  const fetchData = async () => {
+    const postServer = await fetchPosts();
+    setPostList([...postList, ...postServer]);
+    if (postServer.length === 0 || postServer.length < 5) {
+      setNoMore(false);
+    }
+    setPage(page + 1);
+  };
   return (
     <HomeLayout>
       <div className="container">
@@ -42,31 +57,60 @@ const Home = () => {
               {isLoading ? (
                 <Loading visible={isLoading} />
               ) : (
-                postList.map((post) => {
-                  return (
-                    <div className="PostList__form" key={post.id}>
-                      <div className="PostList__form__info-user">
-                        <div>
-                          <img src="https://via.placeholder.com/256x186?fbclid=IwAR18p3QwgMQ0wYEmlIqxKZFbDBTFAhNZD8R4VyH6DxWdI6GULxDei-7L87M" />
-                          <span>Nguyễn Dũng</span>
-                        </div>
-                        <div>
-                          {moment(post.created_at).format("DD/MM/YYYY")}
-                        </div>
+                <InfinitScroll
+                  dataLength={postList.length}
+                  next={fetchData}
+                  hasMore={noMore}
+                  loader={
+                    <div className="text-center">
+                      <div
+                        className="spinner-grow text-light m-3 "
+                        role="status"
+                      >
+                        <span className="sr-only">Loading...</span>
                       </div>
-                      <div className="PostList__form__info-post">
-                        <h5>{post.title}</h5>
-                        <p>Số lượng thành viên {post.members} người</p>
-                        <p>{post.content}</p>
+                      <div
+                        className="spinner-grow text-light m-3"
+                        role="status"
+                      >
+                        <span className="sr-only">Loading...</span>
                       </div>
-                      <div className="PostList__form__see-more">
-                        <Link to={`/post-details/${post.slug}.${post.id}`}>
-                          <Button>Xem thêm</Button>
-                        </Link>
+                      <div
+                        className="spinner-grow text-light m-3"
+                        role="status"
+                      >
+                        <span className="sr-only">Loading...</span>
                       </div>
                     </div>
-                  );
-                })
+                  }
+                  endMessage=""
+                >
+                  {postList.map((post) => {
+                    return (
+                      <div className="PostList__form" key={post.id}>
+                        <div className="PostList__form__info-user">
+                          <div>
+                            <img src="https://via.placeholder.com/256x186?fbclid=IwAR18p3QwgMQ0wYEmlIqxKZFbDBTFAhNZD8R4VyH6DxWdI6GULxDei-7L87M" />
+                            <span>Nguyễn Dũng</span>
+                          </div>
+                          <div>
+                            {moment(post.created_at).format("DD/MM/YYYY")}
+                          </div>
+                        </div>
+                        <div className="PostList__form__info-post">
+                          <h5>{post.title}</h5>
+                          <p>Số lượng thành viên {post.members} người</p>
+                          <p>{post.content}</p>
+                        </div>
+                        <div className="PostList__form__see-more">
+                          <Link to={`/post-details/${post.slug}.${post.id}`}>
+                            <Button>Xem thêm</Button>
+                          </Link>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </InfinitScroll>
               )}
             </div>
           </div>
