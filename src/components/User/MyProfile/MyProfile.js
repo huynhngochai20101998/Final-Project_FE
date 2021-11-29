141d6c353d46470a6ecef060731874eaf3a6dcc0
tamaumi <huynhngochai20101998@gmail.com>
2021-11-29 18:01:14 +0700

    update

---
+++ src/components/User/MyProfile/MyProfile.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./MyProfile.scss";
+
+const MyProfile = () => {
+  return (
+    <div className="profile-wrapper">
+      <div className="profile">
+        <div className="profile-header"></div>
+        <div className="profile-body"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MyProfile;
