a3d15457951fe3ca4ebf00d9861f8380356569e3
Duc Huy <nguyenduchuy548@gmail.com>
2021-12-04 10:45:24 +0700

    sprint-3-message

--- src/pages/Home/PostList.js
+++ src/pages/Home/PostList.js
@@ -14,7 +14,8 @@ function PostList(props) {
     first_name,
     last_name,
     profile_image_url,
-    user_id
+    user_id,
+    active
   } = props.post;
   const history = useHistory();
   const fullName = first_name + " " + last_name;
@@ -22,34 +23,38 @@ function PostList(props) {
     history.push(`/personal-info-user/${user_id}`);
   };
   return (
-    <div className="PostList__form">
-      <div className="PostList__form__info-user">
-        <div>
-          <img src={profile_image_url} onClick={handleToPersonalPage} />
-          <span>{fullName}</span>
+    <>
+      {active === true && (
+        <div className="PostList__form">
+          <div className="PostList__form__info-user">
+            <div>
+              <img src={profile_image_url} onClick={handleToPersonalPage} />
+              <span>{fullName}</span>
+            </div>
+            <div>{moment(created_at).format("DD/MM/YYYY")}</div>
+          </div>
+          <div className="PostList__form__info-post">
+            <h5>{title}</h5>
+            <p>Số lượng thành viên {members} người</p>
+            <p>{content}</p>
+          </div>
+          <div className="PostList__form__see-more">
+            <Link to={`/post-details/${slug}.${id}`}>
+              <Button
+                onClick={() => {
+                  localStorage.setItem(
+                    "postCreationId",
+                    JSON.stringify(props.post.id)
+                  );
+                }}
+              >
+                Xem thêm
+              </Button>
+            </Link>
+          </div>
         </div>
-        <div>{moment(created_at).format("DD/MM/YYYY")}</div>
-      </div>
-      <div className="PostList__form__info-post">
-        <h5>{title}</h5>
-        <p>Số lượng thành viên {members} người</p>
-        <p>{content}</p>
-      </div>
-      <div className="PostList__form__see-more">
-        <Link to={`/post-details/${slug}.${id}`}>
-          <Button
-            onClick={() => {
-              localStorage.setItem(
-                "postCreationId",
-                JSON.stringify(props.post.id)
-              );
-            }}
-          >
-            Xem thêm
-          </Button>
-        </Link>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
--- src/pages/Room/Room.js
+++ src/pages/Room/Room.js
@@ -5,9 +5,17 @@ import TableParticipants from "../../components/CallVideo/TableParticipants";
 import "./Room.scss";
 import { useParams } from "react-router";
 import http from "core/services/httpService";
+import { Form, Formik } from "formik";
+import { FormGroup, Input } from "reactstrap";
+import axios from "axios";
 export default function RoomChat() {
   const path = useParams();
   const [groupData, setGroupData] = useState({});
+  const [messageList, setMessageList] = useState([]);
+  const api = axios.create({
+    baseURL: `https://fathomless-depths-07369.herokuapp.com`
+  });
+  // console.log(groupData);
   useEffect(() => {
     async function getGroupData() {
       try {
@@ -19,6 +27,19 @@ export default function RoomChat() {
     }
     getGroupData();
   }, []);
+  useEffect(() => {
+    async function getMessageData() {
+      try {
+        const response = await api.get(`/api/messages?group_id=${path.id}`);
+        setMessageList(response.data);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    getMessageData();
+  }, []);
+  console.log(messageList);
+  console.log(groupData);
   return (
     <HomeLayout>
       <div className="container-fluid">
@@ -32,7 +53,74 @@ export default function RoomChat() {
               <WhiteBoard wb_id={groupData.wb_id} />
             </div>
           </div>
-          <div className="col-sm-4 col-md-4 col-lg-4"></div>
+          <div className="col-sm-4 col-md-4 col-lg-4">
+            <div className="Message">
+              <div className="Message__title">
+                <h4>Tin nhắn</h4>
+                <div>
+                  <i className="far fa-user-circle"></i>
+                  <span>5</span>
+                </div>
+              </div>
+              <div className="Message__body">
+                <div className="Message__body__title">
+                  <div className="Message__body__title__user">
+                    <img src="https://via.placeholder.com/256x186?fbclid=IwAR18p3QwgMQ0wYEmlIqxKZFbDBTFAhNZD8R4VyH6DxWdI6GULxDei-7L87M" />
+                    <span>Nguyen Dung22</span>
+                  </div>
+                  <div className="Message__body__title__mess">
+                    <p>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Magni, facilis! Consequuntur pariatur recusandae
+                      cupiditate ea autem illo expedita dolorum porro alias
+                      sapiente facere, iste sed optio eum nisi minus nam?
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="Message__chat">
+                <Formik
+                  initialValues={{
+                    group_id: Number(path.id),
+                    content: ""
+                  }}
+                  // validationSchema={validationSchema}
+                  onSubmit={(values, actions) => {
+                    api.post(`/api/messages`, values).then(() => {
+                      actions.setSubmitting(false);
+                      actions.resetForm({
+                        values: {
+                          group_id: Number(path.id),
+                          content: ""
+                        }
+                      });
+                      // setIsLoading(!isLoading);
+                    });
+                  }}
+                >
+                  {(formikProps) => {
+                    const { values, handleChange } = formikProps;
+                    return (
+                      <Form>
+                        <FormGroup>
+                          <Input
+                            name="content"
+                            type="text"
+                            value={values.content}
+                            onChange={handleChange}
+                            placeholder="Typing here"
+                          ></Input>
+                        </FormGroup>
+                        <button type="submit">
+                          <i className="far fa-paper-plane"></i>
+                        </button>
+                      </Form>
+                    );
+                  }}
+                </Formik>
+              </div>
+            </div>
+          </div>
         </div>
         <TableParticipants id={path.id}></TableParticipants>
       </div>
